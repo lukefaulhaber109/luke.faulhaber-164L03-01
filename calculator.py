b46d0da1566b8d0bc2cc03870dc2c421af9f6b9b
luke faulhaber <luke.faulhaber@itas.ca>
2026-02-11 12:54:09 -0800

    Added division function with zero division error handling

--- calculator.py
+++ calculator.py
@@ -17,7 +17,14 @@ if __name__ == "__main__":
     print(f"5 + 3 = {add(5, 3)}")
     print(f"10 - 4 = {subtract(10, 4)}")
 def multiply(a, b):
-    """return the product of two numbers"""
+    """return the product of two numbers."""
     return a * b
 if __name__ == "__main__":
     print(f"6 * 7 = {multiply(6, 7)}")
+def divide(a, b):
+    """return quotient of two numbers."""
+    if b ==0:
+        return "Error: Division by zero"
+    return a / b
+print(f"20 / 4 = {divide(20, 4)}")
+print(f"10 / 0 = {divide(10, 0)}")
